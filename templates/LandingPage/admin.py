b2f3e7fc5bd3db97b763cf4fd5e947640d61950d
Ndeepanshu <46917948+Ndeepanshu@users.noreply.github.com>
2020-06-25 19:37:05 +0530

    third

--- templates/LandingPage/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Signup , OnlineCounselling
-admin.site.register(Signup)
-admin.site.register(OnlineCounselling)
--- templates/facilitators/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FacilitatorsConfig(AppConfig):
-    name = 'facilitators'
